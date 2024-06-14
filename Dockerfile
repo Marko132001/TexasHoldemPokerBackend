5225fd1898b834f5925f7321aa65b4cbc2ba44c1
Marko132001 <markokozlov@gmail.com>
2024-06-15 00:26:20 +0200

    Server port fix. Fetching firebase json file fix.

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ RUN gradle buildFatJar --no-daemon
 FROM openjdk:11
 EXPOSE 80:80
 RUN mkdir /app
+#COPY /src/main/resources/pokerapp-8f562-firebase-adminsdk-7n239-13a746135f.json /app/
 COPY --from=build /home/gradle/src/build/libs/*.jar /app/poker-app_backend.jar
 ENTRYPOINT ["java","-jar","/app/poker-app_backend.jar"]
--- src/main/kotlin/com/backend/firebase/FirebaseAdmin.kt
+++ src/main/kotlin/com/backend/firebase/FirebaseAdmin.kt
@@ -3,12 +3,11 @@ package com.backend.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import java.io.FileInputStream
 import java.io.InputStream
 
 object FirebaseAdmin {
-    private val serviceAccount: InputStream? =
-        this::class.java.classLoader
-            .getResourceAsStream("pokerapp-8f562-firebase-adminsdk-7n239-13a746135f.json")
+    private val serviceAccount: FileInputStream = FileInputStream("/app/pokerapp-8f562-firebase-adminsdk-7n239-13a746135f.json")
 
     private val options: FirebaseOptions = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
